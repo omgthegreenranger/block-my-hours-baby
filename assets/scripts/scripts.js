05495f9f71f34fd4942b5e6f86873c4640090d10
omgthegreenranger <das.shaggy@gmail.com>
2023-01-26 21:10:03 -0500

    updated script

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,6 +8,8 @@ let dayLength = [];
 let scheduleHours = document.querySelector("#hour-boxes");
 let timesArrow = '';
 let schedules = localStorage.schedules ? JSON.parse(localStorage.schedules) : [];
+let timeBlock = $('#hour-boxes');
+
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -15,19 +17,7 @@ let schedules = localStorage.schedules ? JSON.parse(localStorage.schedules) : []
 $('#currentDay').text("Today is " + dayjs().format("MMM DD, YYYY") + ", and here is your schedule");
 $('#currentTime').text("It is currently " + dayjs().format("hh:mm a"));
 
-// TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  
+
 
 // write a script to populate all the hours of the day
 // note that dayStart and dayEnd are variables in order to provide workday-hour select.
@@ -36,11 +26,11 @@ $(function () {
     dayEnd = 17;
     for (i = dayStart; i <= dayEnd; i++) {
         // determine if time is past or present
-        if(i < dayjs().format("hh")) {
+        if(i < dayjs().format("HH")) {
             timesArrow = "past";
-        } else if (i == dayjs().format("hh")) {
+        } else if (i == dayjs().format("HH")) {
             timesArrow = "present";
-        } else if (i > dayjs().format("hh")) {
+        } else if (i > dayjs().format("HH")) {
             timesArrow ="future";
         };
     dayDiv = `<div id="hour-${i}" class="row time-block ${timesArrow}">
@@ -56,11 +46,8 @@ $(function () {
     }
 );
 
-
-let timeBlock = $('#hour-boxes');
-
 timeBlock.on('click', '.saveBtn', function (event) {
     btnClicked = $(event.target);
-    schedules.push([btnClicked.parent().attr('id'),btnClicked.parent().children('textarea').val()]);
+    schedules.push([btnClicked.parent().attr('id'),  btnClicked.parent().children('textarea').val()]);
     localStorage.setItem("schedules", JSON.stringify(schedules));
 });
